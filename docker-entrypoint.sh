2536cc713e40ea00db10c94902d9917a17f2f19b
hotsadboi <hotsadboi@tuta.io>
2026-06-08 18:18:00 +0500

    docker: try to generate migrations no matter what
    this only works if the db container is already running when the app
    container starts. not the best solution, but i cannot think of anything
    better

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
-set -e
+#!/usr/bin/env bash
+node node_modules/typeorm/cli.js migration:generate -d dist/src/typeorm.datasource.js dist/migrations && \
 node node_modules/typeorm/cli.js migration:run -d dist/src/typeorm.datasource.js
 exec node dist/src/main.js
